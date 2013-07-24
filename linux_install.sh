25782c8bda59222487b7e4a4648bcb147589b423
Daniel Black <daniel@mint>
2013-07-24 04:20:47 -0700

    Made it so you don't have to uninstall to update

--- linux_install.sh
+++ linux_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm /usr/bin/openfortune
-cp -rf Fortune\ Files /usr/share/openfortune
+cp Fortune\ Files/*.frtn /usr/share/openfortune
 cp Python\ Version/OpenFortune.py /usr/bin/openfortune
 chmod -R 555 /usr/share/openfortune
 chmod 555 /usr/bin/openfortune
